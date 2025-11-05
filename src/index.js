80d874f105b8b2fdfcfa47b53270cb4331ca1195
EmreKaplan67 <emrekaplan96@gmail.com>
2025-11-05 16:12:39 +0000

    plain html and css

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import "./styles.css";
+
+document.getElementById("content").textContent = "Hello World";
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    port: 3000,
+    open: true,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
